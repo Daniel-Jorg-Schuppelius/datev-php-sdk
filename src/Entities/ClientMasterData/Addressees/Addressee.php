4b57b5617d27b26ca500e38160d62e5b4b9bee30
Daniel Jörg Schuppelius <schuppeliusd@schuppelius-gierke.org>
2024-10-04 12:15:28 +0200

    add addresseesendpoint and tests, add forgoten properties to addressee

--- src/Entities/ClientMasterData/Addressees/Addressee.php
+++ src/Entities/ClientMasterData/Addressees/Addressee.php
@@ -7,12 +7,18 @@ namespace Datev\Entities\ClientMasterData\Addressees;
 use APIToolkit\Contracts\Abstracts\NamedEntity;
 use APIToolkit\Contracts\Interfaces\NamedEntityInterfaces\IdentifiableNamedEntityInterface;
 use DateTime;
+use Datev\Entities\ClientMasterData\Addresses\Addresses;
+use Datev\Entities\ClientMasterData\BankAccounts\BankAccounts;
+use Datev\Entities\ClientMasterData\Communications\Communications;
 use Datev\Entities\ClientMasterData\CompanyNames\CompanyName;
 use Datev\Entities\ClientMasterData\CompanyNames\CompanyNames;
+use Datev\Entities\ClientMasterData\ContactPersons\ContactPersons;
+use Datev\Entities\ClientMasterData\Details\Detail;
 use Datev\Entities\ClientMasterData\LegalFormIDs\LegalFormIDs;
 use Datev\Entities\ClientMasterData\ShortNames\ShortNames;
 use Datev\Entities\ClientMasterData\Surnames\Surname;
 use Datev\Entities\ClientMasterData\Surnames\Surnames;
+use Datev\Entities\ClientMasterData\TaxOffices\TaxOffices;
 use Datev\Enums\Status;
 use Psr\Log\LoggerInterface;
 
@@ -24,9 +30,11 @@ class Addressee extends NamedEntity implements IdentifiableNamedEntityInterface 
     protected ?string $current_short_name;
     protected ?Status $status;
     protected ?string $surrogate_name;
+    protected ?DateTime $timestamp;
     protected ?string $type;
     protected ?DateTime $date_of_birth;
     protected ?string $etin;
+    protected ?string $firstname;
     protected ?string $sex;
     protected ?Surnames $surnames;
     protected ?Surname $current_surname;
@@ -36,6 +44,12 @@ class Addressee extends NamedEntity implements IdentifiableNamedEntityInterface 
     protected ?DateTime $date_of_foundation;
     protected ?LegalFormIDs $legal_form_ids;
     protected ?LegalFormID $current_legal_form_id;
+    protected ?Detail $detail;
+    protected ?Addresses $addresses;
+    protected ?Communications $communications;
+    protected ?BankAccounts $bank_accounts;
+    protected ?TaxOffices $tax_offices;
+    protected ?ContactPersons $contact_persons;
 
     public function __construct($data = null, ?LoggerInterface $logger = null) {
         parent::__construct($data, $logger);
---
+++ tests/Endpoints/ClientMasterData/AddresseesTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Endpoints\Diagnostics;
+
+use Datev\API\Desktop\Endpoints\ClientMasterData\AddresseesEndpoint;
+use Datev\Entities\ClientMasterData\Addressees\Addressee;
+use Datev\Entities\ClientMasterData\Addressees\Addressees;
+use Tests\Contracts\EndpointTest;
+
+class AddresseesTest extends EndpointTest {
+    protected ?AddresseesEndpoint $endpoint;
+
+    public function __construct($name) {
+        parent::__construct($name);
+        $this->endpoint = new AddresseesEndpoint($this->client, $this->logger);
+        $this->apiDisabled = true; // API is disabled
+    }
+
+    public function testGetAddressees() {
+        if ($this->apiDisabled) {
+            $this->markTestSkipped('API is disabled');
+        }
+
+        $addressees = $this->endpoint->search();
+        $this->assertInstanceOf(Addressees::class, $addressees);
+        $this->assertNotEmpty($addressees->getValues(), "No addressees found");
+        $randomaddressee = $addressees->getValues()[array_rand($addressees->getValues())];
+        $this->assertInstanceOf(Addressee::class, $randomaddressee);
+        $addresse = $this->endpoint->get($randomaddressee->getId());
+        $this->assertInstanceOf(Addressee::class, $addresse);
+        $this->assertEquals($randomaddressee->getId(), $addresse->getId());
+    }
+}
