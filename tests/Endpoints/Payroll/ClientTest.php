4ff0024864e345eb85df16accbad8c86c5aaf4f1
DSchuppelius <daniel.schuppelius@gmail.com>
2025-01-04 09:33:59 +0100

    disable payroll api test

--- tests/Endpoints/Payroll/ClientTest.php
+++ tests/Endpoints/Payroll/ClientTest.php
@@ -22,7 +22,7 @@ class ClientTest extends EndpointTest {
     public function __construct($name) {
         parent::__construct($name);
         $this->endpoint = new ClientsEndpoint($this->client, $this->logger);
-        $this->apiDisabled = false; // API is disabled
+        $this->apiDisabled = true; // API is disabled
     }
 
     public function testJsonSerialize() {
