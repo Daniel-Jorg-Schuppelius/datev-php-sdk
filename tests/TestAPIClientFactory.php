a3281934c6bb241dc1b25e320dcf216a77c58509
DSchuppelius <daniel.schuppelius@gmail.com>
2025-03-11 11:56:54 +0100

    add config toolkit

--- tests/TestAPIClientFactory.php
+++ tests/TestAPIClientFactory.php
@@ -11,17 +11,18 @@
 namespace Tests;
 
 use APIToolkit\Contracts\Interfaces\API\ApiClientInterface;
+use ConfigToolkit\ConfigLoader;
 use ERRORToolkit\Factories\ConsoleLoggerFactory;
 use Datev\API\Desktop\ClientBasicAuth;
-use Tests\Config\Config;
 
 class TestAPIClientFactory {
     private static ?ApiClientInterface $client = null;
 
     public static function getClient(): ApiClientInterface {
         if (self::$client === null) {
-            $config = new Config();
-            self::$client = new ClientBasicAuth($config->user, $config->password, $config->resourceUrl ?? "https://127.0.0.1:58452", ConsoleLoggerFactory::getLogger());
+            $config = ConfigLoader::getInstance();
+            $config->loadConfigFile(__DIR__ . "/../.samples/config.json");
+            self::$client = new ClientBasicAuth($config->get("DATEV-DESKTOP-API", "user"), $config->get("DATEV-DESKTOP-API", "password"), $config->get("DATEV-DESKTOP-API", "resourceurl", "https://127.0.0.1:58452"), ConsoleLoggerFactory::getLogger());
         }
         return self::$client;
     }
